0de65578e7416c1263999cdbfcb579b159cb4665
Juwel-771 <juwelah99@gmail.com>
2024-02-13 03:44:42 +0600

    base-height-button-js-connected

--- script/trianlge.js
+++ script/trianlge.js
@@ -1,3 +1,17 @@
-document.getElementById('triangle').addEventListener('click', function connection(){
-    alert("Connected");
-})
+// document.getElementById('triangle').addEventListener('click', function connection(){
+//     alert("Connected");
+// })
+
+function calculateTriangleArea(){
+    const triangleText = document.getElementById('triangle-base');
+    const triangleInputValue = triangleText.value;
+    const triangleBase = parseFloat(triangleInputValue);
+
+    // console.log(triangleBase);
+
+    const triangleHeightText = document.getElementById('triangle-height');
+    const triangleHeightValue = triangleHeightText.value;
+    const triangleHeight = parseFloat(triangleHeightValue);
+    console.log(triangleHeight);
+}
+
