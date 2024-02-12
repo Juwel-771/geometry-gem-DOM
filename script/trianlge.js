5c01566c982f670c0deaa62c6f7eb88b9543e4da
Juwel-771 <juwelah99@gmail.com>
2024-02-13 04:10:41 +0600

    triangle completed

--- script/trianlge.js
+++ script/trianlge.js
@@ -3,15 +3,22 @@
 // })
 
 function calculateTriangleArea(){
+    // get triangle base value
     const triangleText = document.getElementById('triangle-base');
     const triangleInputValue = triangleText.value;
     const triangleBase = parseFloat(triangleInputValue);
-
     // console.log(triangleBase);
 
+    // get triangle height value
     const triangleHeightText = document.getElementById('triangle-height');
     const triangleHeightValue = triangleHeightText.value;
     const triangleHeight = parseFloat(triangleHeightValue);
-    console.log(triangleHeight);
+    // console.log(triangleHeight);
+
+    const triangleArea = 0.5 * triangleBase *triangleHeight;
+    console.log("Area of the triangle: ",triangleArea);
+
+    const triangleAreaShow = document.getElementById('area-result');
+    triangleAreaShow.innerText = triangleArea;
 }
 
